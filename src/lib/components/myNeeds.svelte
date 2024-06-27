48ff5cc2a70ab6e73473c15a8d663cccbbfeb3cd
Nathan Petrangelo <nathan@petrangelo.org>
2024-06-27 00:40:47 -0700

    Added MyNeeds

---
+++ src/lib/components/myNeeds.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+    import { Input } from "./ui/input";
+    import { ScrollArea } from "./ui/scroll-area";
+    import { Separator } from "./ui/separator";
+    type Props = {entries: Array<string>};
+    let {entries = $bindable([])}: Props = $props();
+    let input = $state("");
+
+    function onsubmit() {
+        entries.push(input);
+    }
+  </script>
+  
+  <h1 class="my-2">Things I Need</h1>
+  <form {onsubmit} class="mb-4">
+    <Input type="text" placeholder="Hit Enter to add need" class="w-auto" bind:value={input}></Input>
+  </form>
+  <ScrollArea class="h-48 w-48 rounded-md border">
+      <ul>
+          {#each entries as entry}
+          <li class="text-sm my-2">
+              {entry}
+          </li>
+          <Separator/>
+      {/each}
+      </ul>
+  </ScrollArea>
+  
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,14 @@
 <script lang="ts">
     import Ledger from "$lib/components/ledger.svelte";
-  import OtherNeeds from "$lib/components/otherNeeds.svelte";
-    const entries = Array.from({ length: 50 }).map(
-    (_, i, a) => `v1.2.0-beta.${a.length - i}`
-  );
+    import MyNeeds from "$lib/components/myNeeds.svelte";
+    import OtherNeeds from "$lib/components/otherNeeds.svelte";
+    let entries = $state(Array.from({ length: 3 }).map(
+        (_, i, a) => `v1.2.0-beta.${a.length - i}`
+    ));
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<MyNeeds bind:entries={entries}></MyNeeds>
 <OtherNeeds name="Harley" {entries}></OtherNeeds>
 <Ledger {entries}></Ledger>
