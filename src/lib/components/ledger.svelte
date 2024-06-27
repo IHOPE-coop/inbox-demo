e88fbc63338459508df079cf262440b1ccd2fe6e
Nathan Petrangelo <nathan@petrangelo.org>
2024-06-26 23:05:27 -0700

    Added Ledger

---
+++ src/lib/components/ledger.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import { ScrollArea } from "./ui/scroll-area";
+  import { Separator } from "./ui/separator";
+  type Props = {entries: Array<string>};
+  let {entries}: Props = $props();
+</script>
+
+<h1 class="my-2">Ledger</h1>
+<ScrollArea class="h-48 w-48 rounded-md border">
+    <ul>
+        {#each entries as entry}
+        <li class="text-sm my-2">
+            {entry}
+        </li>
+        <Separator/>
+    {/each}
+    </ul>
+</ScrollArea>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
+<script lang="ts">
+    import Ledger from "$lib/components/ledger.svelte";
+    const entries = Array.from({ length: 50 }).map(
+    (_, i, a) => `v1.2.0-beta.${a.length - i}`
+  );
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<Ledger {entries}></Ledger>
