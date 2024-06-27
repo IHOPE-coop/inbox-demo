60ab1b9c9a91e390a4706822c6e48549502e6921
Nathan Petrangelo <nathan@petrangelo.org>
2024-06-26 23:09:36 -0700

    Added OtherNeeds

---
+++ src/lib/components/otherNeeds.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    import { ScrollArea } from "./ui/scroll-area";
+    import { Separator } from "./ui/separator";
+    type Props = {name: string, entries: Array<string>};
+    let {name, entries}: Props = $props();
+  </script>
+  
+  <h1 class="my-2">Things {name} Needs</h1>
+  <ScrollArea class="h-48 w-48 rounded-md border">
+      <ul>
+          {#each entries as entry}
+          <li class="text-sm my-2">
+              {entry}
+          </li>
+          <Separator/>
+      {/each}
+      </ul>
+  </ScrollArea>
+  
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     import Ledger from "$lib/components/ledger.svelte";
+  import OtherNeeds from "$lib/components/otherNeeds.svelte";
     const entries = Array.from({ length: 50 }).map(
     (_, i, a) => `v1.2.0-beta.${a.length - i}`
   );
@@ -7,4 +8,5 @@
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<OtherNeeds name="Harley" {entries}></OtherNeeds>
 <Ledger {entries}></Ledger>
